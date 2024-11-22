665160a5733ec50b74fa216dd916aa146f8ca29c
NatãNunes2C <00001089980450sp@al.educacao.sp.gov.br>
2024-11-22 08:19:51 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -17,6 +17,37 @@ criaCartao(
 )
 
 criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+
+criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
