a8f5a4be05e6b1b565275f0778e6cbb176ed47e5
NatãNunes2C <00001089980450sp@al.educacao.sp.gov.br>
2024-11-22 08:09:27 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -5,47 +5,47 @@ criaCartao(
 )
 
 criaCartao(
-    'Tema CSS',
+ 
     'Para que serve o id no CSS?',
     'Estilizar um elemento único'
 )
 
 criaCartao(
-    'Tema CSS',
+
     'Como mudar a cor de fundo com CSS?',
     'Use background-color'
 )
 
 criaCartao(
-    'Tema HTML',
+
     ' Para que serve a tag <a>?',
     'Criar links'
 )
 
 criaCartao(
-    'Tema HTML',
+
     ' Para que serve a tag <a>?',
     'Criar links'
 
 criaCartao(
-    'Tema HTML',
+    
     ' O que é uma tag <div>?',
     'Um contêiner de bloco para agrupar elementos'
 
 criaCartao(
-    'Tema JavaScript',
+  
     'O que é JavaScript?,
     'Linguagem para adicionar interatividade em páginas web'
 )
 
 criaCartao(
-    'Tema JavaScript',
+ 
     'Como exibir uma mensagem de alerta?',
     'Use alert()'
 )
 
 criaCartao(
-    'Tema JavaScript',
+    
     ' Para que serve o let no JavaScript?',
     'Declarar variáveis com escopo de bloco'
 )
